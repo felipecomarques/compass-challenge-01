77b5f9cd99a421be9a32ed12ffad58041de5aac8
Felipe Marques <felipe.comarques1208@gmail.com>
2023-06-01 13:30:19 -0300

    chore: Create a data file

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
-const nome = 'Felipe';
-console.log(`Hello ${nome}`);
+import express, { Request, Response } from 'express';
+import tutors from './data';
+
+const app = express();
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
