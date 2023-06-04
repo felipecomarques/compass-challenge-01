c12d21ac5e30cfb8a47e8849384fac5d96125ee7
Felipe Marques <felipe.comarques1208@gmail.com>
2023-06-04 19:48:37 -0300

    feat: Implement POST, PUT, and DELETE methods for pets

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
-import express, { Request, Response } from 'express';
-import * as tutorController from './controller/tutors';
-import * as petController from './controller/pet';
+import express, { Request, Response } from "express";
+import * as tutorController from "./controller/tutors";
+import * as petController from "./controller/pet";
 
 const app = express();
 app.use(express.json());
 
 // -- Home Page -- //
-app.get('/', (req: Request, res: Response) => { res.send('Compass') });
+app.get("/", (req: Request, res: Response) => {
+  res.send("Compass");
+});
 
 // -- Tutors Route -- //
-app.get('/tutors', tutorController.getAll);
+app.get("/tutors", tutorController.getAll);
 //app.get('/tutors/:id', tutorController.getByID);
-app.post('/tutors', tutorController.createTutor);
-app.put('/tutors/:id', tutorController.updateTutor);
-app.delete('/tutors/:id', tutorController.deleteTutor);
+app.post("/tutors", tutorController.createTutor);
+app.put("/tutors/:id", tutorController.updateTutor);
+app.delete("/tutors/:id", tutorController.deleteTutor);
 
 // -- Pet Route -- //
-app.post('/pet', petController.createPet);
-app.put('/pet/:tutorId', petController.updatePet);
-app.delete('/pet/:tutorId', petController.deletePet);
+app.post("/pet/:tutorId", petController.createPet);
+app.put("/pet/:petId/tutor/:tutorId", petController.updatePet);
+app.delete("/pet/:petId/tutor/:tutorId", petController.deletePet);
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });
--- src/controller/pet.ts
+++ src/controller/pet.ts
@@ -1,17 +1,56 @@
-import { Request, Response } from 'express';
-import tutors from '../data/dados.json'; 
+import { Request, Response } from "express";
+import tutors from "../data/dados.json";
 
-// POST/pet/:tutor -> > Creates a pet and adds it to.
+// POST/pet/:tutor -> Creates a pet and adds it to.
 export const createPet = (req: Request, res: Response) => {
+  const tutorId = parseInt(req.params.tutorId, 10);
+  const newPet = req.body;
 
-}
+  const tutor = tutors.find((tutor) => tutor.id === tutorId);
+
+  if (tutor) {
+    const newPetId = tutor.pets.length + 1;
+    newPet.id = newPetId;
+    tutor.pets.push(newPet);
+    res.status(201).json(newPet);
+  } else {
+    res.status(404).json({ message: "Tutor not found" });
+  }
+};
 
 // PUT/pet/:petId/tutor/:tutorId -> updates a pet's info.
 export const updatePet = (req: Request, res: Response) => {
-    
-}
+  const tutorId = parseInt(req.params.tutorId, 10);
+  const petId = parseInt(req.params.petId, 10);
+  const updatedPet = req.body;
+
+  const tutor = tutors.find((tutor) => tutor.id === tutorId);
+
+  if (tutor) {
+    const pet = tutor.pets.find((pet) => pet.id === petId);
+    if (pet) {
+      Object.assign(pet, updatedPet);
+      res.json(pet);
+    } else res.status(404).json({ message: "Pet not found" });
+  } else res.status(404).json({ message: "Tutor not found" });
+};
 
 // DELETE/pet/:petId/tutor/:tutorI -> deletes a pet from a tutor.
 export const deletePet = (req: Request, res: Response) => {
-    
-}
+  const tutorId = parseInt(req.params.tutorId, 10);
+  const petId = parseInt(req.params.petId, 10);
+
+  const tutor = tutors.find((tutor) => tutor.id === tutorId);
+
+  if (tutor) {
+    const petIndex = tutor.pets.findIndex((pet) => pet.id === petId);
+    if (petIndex !== -1) {
+      const deletedPet = tutor.pets.splice(petIndex, 1);
+      res.json(deletedPet[0]);
+    } else {
+      res.status(404).json({ message: "Pet not found" });
+    }
+  } else {
+    res.status(404).json({ message: "Tutor not found" });
+  }
+};
